60725d2575797aa72d5d1c9b6f7c689ac82dcd05
Tom Wayson <tom@tomwayson.com>
2014-03-07 08:31:07 -0800

    added esri config, code, and specs. map tests not working

---
+++ spec/imageServiceUtilsSpec.js
@@ -0,0 +1,82 @@
+define([
+  'dojo/dom-construct',
+  'esri/map',
+  'esri-utils/imageServiceUtils'
+], function(
+  domConstruct,
+  Map,
+  imageServiceUtils
+) {
+
+  xdescribe('when setting layer url', function() {
+    var map;
+    var url;
+
+    // runs before each spec
+    beforeEach(function() {
+      // test service
+      url = "http://imagery.arcgisonline.com/arcgis/rest/services/LandsatGLS/FalseColor/ImageServer";
+      // init map
+      var div = domConstruct.create('div', {style: 'width:300px;height:200px'});
+      map = new Map(div, {
+          basemap: "topo",
+          center: [-122.45, 37.75],
+          zoom: 13,
+          sliderStyle: "small"
+      });
+    });
+
+    // runs after each spec
+    afterEach(function() {
+      // clear map
+      map.destroy();
+      map = null;
+    });
+
+    // specs
+    it("should add the layer if not already added", function(done) {
+      var options = {id: 'myLayerId'},
+        myLayer;
+      map.on('layer-add-result', function(e) {
+        console.log(e.layer.id, e.layer.url);
+        if (e.layer.url === url) {
+          myLayer = e.layer;
+          expect(myLayer).toBeDefined();
+          expect(myLayer.id).toEqual(options.id);
+          expect(myLayer.url).toEqual(url);
+          done();
+        }
+      });
+      imageServiceUtils.setUrl(map, url, options);
+    });
+
+    it("should replace the layer if already added", function(done) {
+      var options = {id: 'myLayerId'},
+        replaceUrl = 'http://imagery.arcgisonline.com/arcgis/rest/services/LandsatGLS/GLS2010_Enhanced/ImageServer',
+        myLayer;
+      // after each layer has been added
+      map.on('layer-add-result', function(e) {
+        console.log(e.layer.id, e.layer.url);
+        if (e.layer.url === url) {
+          // layer added for the first time
+          myLayer = e.layer;
+          expect(myLayer).toBeDefined();
+          expect(myLayer.id).toEqual(options.id);
+          expect(myLayer.url).toEqual(url);
+
+          // now let's update the layer and re test
+          imageServiceUtils.setUrl(map, replaceUrl, options);
+        } else if (e.layer.url === replaceUrl) {
+          // second time
+          // - id should be same, but url updated
+          myLayer = e.layer;
+          expect(myLayer).toBeDefined();
+          expect(myLayer.id).toEqual(options.id);
+          expect(myLayer.url).toEqual(replaceUrl);
+          done();
+        }
+      });
+      imageServiceUtils.setUrl(map, url, options);
+    });
+  });
+});
